bf5fb276854650ac71c5afcfd62f04d9ded09456
Milton Restrepo <miltonjrv@gmail.com>
2022-10-25 17:03:20 -0500

    Add project files.

---
+++ ParqueaderoMatt/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParqueaderoMatt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Administración del parqueadero de Mattelsa sede central.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Información de contacto.";
+
+            return View();
+        }
+    }
+}
---
+++ ParqueaderoMatt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParqueaderoMatt.MvcApplication" Language="C#" %>
